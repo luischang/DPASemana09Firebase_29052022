bfc8146ed259980df2ce1d1833ec6b00024adf7b
Luis Chang <luis.chang@iugo.com.uy>
2022-06-05 09:14:49 -0500

    List of Course

--- app/src/main/java/dev/lchang/dpasemana09firebase/ui/fragments/ListadoFragment.kt
+++ app/src/main/java/dev/lchang/dpasemana09firebase/ui/fragments/ListadoFragment.kt
@@ -1,11 +1,18 @@
 package dev.lchang.dpasemana09firebase.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 import dev.lchang.dpasemana09firebase.R
+import dev.lchang.dpasemana09firebase.ui.fragments.adapter.CourseAdapter
+import dev.lchang.dpasemana09firebase.ui.fragments.model.CourseModel
 
 class ListadoFragment : Fragment() {
 
@@ -14,7 +21,37 @@ class ListadoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listado, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_listado, container, false)
+        val db = FirebaseFirestore.getInstance()
+        val lstCourses: ArrayList<CourseModel> = ArrayList()
+        val rvCourse: RecyclerView = view.findViewById(R.id.rvCourse)
+
+        db.collection("courses")
+            .addSnapshotListener{snapshots, e->
+                if(e!=null){
+                    Log.e("Firebase Error","Ocurrió un error al cargar los cursos")
+                    return@addSnapshotListener
+                }
+
+                for(dc in snapshots!!.documentChanges){
+                    when(dc.type){
+                        DocumentChange.Type.ADDED,
+                            DocumentChange.Type.MODIFIED,
+                            DocumentChange.Type.REMOVED ->{
+                                lstCourses.add(
+                                    CourseModel(dc.document.data["description"].toString(),
+                                                dc.document.data["score"].toString())
+                                )
+                            }
+                    }
+                }
+                rvCourse.adapter = CourseAdapter(lstCourses)
+                rvCourse.layoutManager = LinearLayoutManager(requireContext())
+
+            }
+
+
+        return view
     }
 
 }
--- app/src/main/java/dev/lchang/dpasemana09firebase/ui/fragments/adapter/CourseAdapter.kt
+++ app/src/main/java/dev/lchang/dpasemana09firebase/ui/fragments/adapter/CourseAdapter.kt
@@ -1,4 +1,35 @@
 package dev.lchang.dpasemana09firebase.ui.fragments.adapter
 
-class CourseAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import dev.lchang.dpasemana09firebase.R
+import dev.lchang.dpasemana09firebase.ui.fragments.model.CourseModel
+
+class CourseAdapter(private var lstCourse: List<CourseModel>)
+    : RecyclerView.Adapter<CourseAdapter.ViewHolder>()
+{
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val tvCourse: TextView = itemView.findViewById(R.id.tvCourse)
+        val tvScore: TextView = itemView.findViewById(R.id.tvScore)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_course,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+       val item = lstCourse[position]
+        holder.tvCourse.text = item.description
+        holder.tvScore.text = item.score
+    }
+
+    override fun getItemCount(): Int {
+        return lstCourse.size
+    }
+
+
 }
